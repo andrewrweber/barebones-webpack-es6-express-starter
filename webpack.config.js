b49b7ffb49904007da6bde72b0f96c43c8fdbb93
Andrew Weber <weber@M08.local>
2017-04-10 10:04:39 -0700

    adds sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
         test: /\.html$/,
         loader: 'html-loader'
       }
